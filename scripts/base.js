9cb5730345090746e4b5e38f2e93ed045bfffc65
sadiebybee <byb22004@byui.edu>
2026-01-13 17:16:27 -0700

    head and content finished

---
+++ scripts/base.js
@@ -0,0 +1,8 @@
+// Navigation and Menu
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
