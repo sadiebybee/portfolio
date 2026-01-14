b58305d16343e0236cf9456c40f2b45e09b56b6f
sadiebybee <byb22004@byui.edu>
2026-01-14 00:10:10 -0700

    Home Page Finished

--- scripts/base.js
+++ scripts/base.js
@@ -3,6 +3,42 @@ const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 
 hamButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    hamButton.classList.toggle("open");
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
 });
+
+// Display Copyright
+function getCopyrightYear() {
+  const year = new Date().getFullYear();
+  return `&copy; ${year}`;
+}
+document.getElementById("cYear").innerHTML = getCopyrightYear();
+
+// // Display Last Modified
+// const options = {
+//   month: "short",
+//   day: "numeric",
+//   year: "numeric",
+//   hour: "2-digit",
+//   minute: "2-digit",
+//   second: "2-digit",
+//   hour12: true,
+// };
+
+// function getLastModified() {
+//   const lastModified = new Date(document.lastModified).toLocaleDateString(
+//     "en-US",
+//     options
+//   );
+//   return `Last Modified: ${lastModified}`;
+// }
+// document.getElementById("lastModified").innerHTML = getLastModified();
+
+// Project Directory Page
+
+// Project title
+// Short description
+// Technologies used
+// Screenshot image?
+// Live demo link
+// GitHub repo link
